e127d1919d6730c78d620faabc40500d7c9f6b7d
smileInLife <38873832+codersfarm@users.noreply.github.com>
2019-12-10 16:40:14 +0800

    优化代码

--- elasticsearch6.x/src/main/java/com/elastic/utils/ConfigUtils.java
+++ elasticsearch6.x/src/main/java/com/elastic/utils/ConfigUtils.java
@@ -1,9 +1,6 @@
 package com.elastic.utils;
 
 
-import com.elastic.exception.ESIoException;
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
-
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -13,65 +10,66 @@ import java.util.Properties;
  **/
 public class ConfigUtils {
 
-    /**
-     * 配置文件
-     * */
-    private static String esConfigFileName = "elasticsearch.properties";
+	/**
+	 * 配置文件
+	 */
+	private static String esConfigFileName = "elasticsearch.properties";
 
-    /**
-     * es集群名
-     * */
-    private static String esClusterName;
-    /**
-     * es集群ip地址
-     * */
-    private static String esClusterDiscoverHostName;
-    /**
-     * es集群是否加入嗅探功能
-     * */
-    private static String clientTransportSniff;
+	/**
+	 * es集群名
+	 */
+	private static String esClusterName;
+	/**
+	 * es集群ip地址
+	 */
+	private static String esClusterDiscoverHostName;
+	/**
+	 * es集群是否加入嗅探功能
+	 */
+	private static String clientTransportSniff;
 
-    private static Properties properties = new Properties();
-    static{
-        try {
-            ClassLoader classLoader = ConfigUtils.class.getClassLoader();
-            InputStream resourceAsStream = classLoader.getResourceAsStream(esConfigFileName);
-            properties.load(resourceAsStream);
-            init();
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+	private static Properties properties = new Properties();
 
-    }
+	static {
+		try {
+			ClassLoader classLoader = ConfigUtils.class.getClassLoader();
+			InputStream resourceAsStream = classLoader.getResourceAsStream(esConfigFileName);
+			properties.load(resourceAsStream);
+			init();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-    private static void init() {
+	}
 
-        esClusterName = properties.getProperty("elastic.cluster.name");
-        esClusterDiscoverHostName = properties.getProperty("elastic.cluster.discover.hostname");
-        clientTransportSniff = properties.getProperty("elastic.cluster.clientTransportSniff");
+	private static void init() {
 
-        if ("".equals(esClusterName)||"".equals(esClusterName)||"".equals(clientTransportSniff)){
-            throw new RuntimeException("elasticsearch 集群参数为空异常");
-        }
-    }
+		esClusterName = properties.getProperty("elastic.cluster.name");
+		esClusterDiscoverHostName = properties.getProperty("elastic.cluster.discover.hostname");
+		clientTransportSniff = properties.getProperty("elastic.cluster.clientTransportSniff");
 
-    public static String getEsClusterName() {
-        return esClusterName;
-    }
+		if ("".equals(esClusterName) || "".equals(esClusterName) || "".equals(clientTransportSniff)) {
+			throw new RuntimeException("elasticsearch 集群参数为空异常");
+		}
+	}
 
-    public static String getEsClusterDiscoverHostName() {
-        return esClusterDiscoverHostName;
-    }
+	public static String getEsClusterName() {
+		return esClusterName;
+	}
 
-    public static void setEsClusterDiscoverHostName(String esClusterDiscoverHostName) {
-        ConfigUtils.esClusterDiscoverHostName = esClusterDiscoverHostName;
-    }
+	public static String getEsClusterDiscoverHostName() {
+		return esClusterDiscoverHostName;
+	}
 
-    public static String getClientTransportSniff() {
-        return clientTransportSniff;
-    }
+	public static void setEsClusterDiscoverHostName(String esClusterDiscoverHostName) {
+		ConfigUtils.esClusterDiscoverHostName = esClusterDiscoverHostName;
+	}
 
-    public static void setClientTransportSniff(String clientTransportSniff) {
-        ConfigUtils.clientTransportSniff = clientTransportSniff;
-    }
+	public static String getClientTransportSniff() {
+		return clientTransportSniff;
+	}
+
+	public static void setClientTransportSniff(String clientTransportSniff) {
+		ConfigUtils.clientTransportSniff = clientTransportSniff;
+	}
 }
