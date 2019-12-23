36c2b8f141168de4793996167cef6c89b01ed9a5
smileInLife <38873832+codersfarm@users.noreply.github.com>
2019-12-23 18:00:12 +0800

    增加部分内容

--- elasticsearchAction/src/main/java/com/elastic/operations/search/UserElasticSearchResposity.java
+++ elasticsearchAction/src/main/java/com/elastic/operations/search/UserElasticSearchResposity.java
@@ -9,4 +9,8 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class UserElasticSearchResposity extends PublicElasticSearchResposity<UserESEnity> {
 
+	public void searchScroll (){
+
+	}
+
 }
